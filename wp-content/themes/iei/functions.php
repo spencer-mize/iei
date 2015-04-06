9f2b80998cd69bb23e6f031ea69425098e637de9
Spencer Mize <spencer.mize@ambassador-enterprises.com>
2015-04-06 10:45:19 -0400

    Getting close to IEI revision 2

--- wp-content/themes/iei/functions.php
+++ wp-content/themes/iei/functions.php
@@ -11,6 +11,25 @@ add_theme_support( 'menus' );
 /* Allow Page Excerpts */
 add_post_type_support('page','excerpt');
 
+/* Remove CF7 Consta-load */
+add_filter( 'wpcf7_load_js', '__return_false' );
+add_filter( 'wpcf7_load_css', '__return_false' );
+
+add_filter('timber_context', 'add_wpcf7');
+function add_wpcf7($context){
+	$context['posts'] = Timber::get_posts();
+	if($context['posts'][0]->post_name=="get-involved"){
+		if ( function_exists( 'wpcf7_enqueue_scripts' ) ) {
+			wpcf7_enqueue_scripts();
+		}
+	 
+		if ( function_exists( 'wpcf7_enqueue_styles' ) ) {
+			wpcf7_enqueue_styles();
+		}		
+	}
+	return $context;
+}
+
 /* Setup Admin Menus */
 add_action('admin_menu', 'remove_menus');
 function remove_menus () {
@@ -33,6 +52,12 @@ function add_to_context($context){
 	if($context['posts'][0]->post_name=="our-partners"){
 		$context['partners'] = Timber::get_posts(array('post_type' => 'partners', 'nopaging' => true));
 	}
+	if($context['posts'][0]->post_name=="our-team"){
+		$context['members'] = Timber::get_posts(array('post_type' => 'board', 'nopaging' => true, 'orderby' => 'menu_order' ));
+	}
+	if($context['posts'][0]->post_name=="our-work"){
+		$context['members'] = Timber::get_posts(array('post_type' => 'work', 'nopaging' => true, 'orderby' => 'menu_order' ));
+	}		
 	
 	return $context;
 }	
@@ -45,33 +70,6 @@ function my_remove_meta_boxes() {
 	}
 }
 add_action( 'add_meta_boxes', 'my_remove_meta_boxes' );
-
-/* Fix ACF Directory
-function acf_admin_enqueue_scripts(){
-	global $wp_scripts;var_dump($wp_scripts);
-	$acf = "/advanced-custom-fields/";
-	wp_deregister_script('acf-field-group');
-	wp_deregister_script('acf-input');
-	
-	wp_deregister_style('acf-field-group');
-	wp_deregister_style('acf');
-	wp_deregister_style('acf-global');
-	wp_deregister_style('acf-input');
-	wp_deregister_style('acf-datepicker');
-	
-	wp_enqueue_script( 'acf-field-group',plugins_url() . $acf . "js/field-group.min.js" );
-	wp_enqueue_script( 'acf-input',plugins_url() . $acf . "js/input.min.js" );
-	
-	wp_enqueue_style( 'acf',plugins_url() . $acf . "css/acf.css" );	
-	wp_enqueue_style( 'acf-global',plugins_url() . $acf . "css/global.css" );	
-	wp_enqueue_style( 'acf-field-group',plugins_url() . $acf . "css/field-group.css" );
-	wp_enqueue_style( 'acf-input',plugins_url() . $acf . "css/input.css" );
-	wp_enqueue_style( 'acf-datepicker',plugins_url() . $acf . "core/fields/date_picker/style.date_picker.css" );
-
-}
-add_action('acf/field_group/admin_enqueue_scripts', 'acf_admin_enqueue_scripts');
-add_action('acf/input/admin_enqueue_scripts', 'acf_admin_enqueue_scripts');
- */
  
 /* Add SASS theme CSS */
 add_action( 'wp_enqueue_scripts', 'enqueue_theme_css' );
