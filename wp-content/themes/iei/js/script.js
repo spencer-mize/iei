2e90b63de19c09f7206c7d3afb29f427f5352ac7
Spencer Mize <spencer.mize@ambassador-enterprises.com>
2016-05-02 09:55:51 -0400

    good now?

--- wp-content/themes/iei/js/script.js
+++ wp-content/themes/iei/js/script.js
@@ -2,12 +2,11 @@ $(document).ready(function(){
 	$("body").addClass("loaded");
 	
 	$(window).on("resize",function(){
-		$(".home iframe.video").height($(".home .carousel").height());
 		var height = $(".home .image").height();
 		if(height<200){
-			alert();
 			height = 488
 		}
 		$(".home .carousel").height(height);
+		$(".home iframe.video").height($(".home .carousel").height());		
 	}).trigger("resize");
 });
