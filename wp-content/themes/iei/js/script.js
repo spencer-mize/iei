1e4091ea47c658eeaca82f31bf6f7f36a4225fd9
Spencer Mize <spencer.mize@ambassador-enterprises.com>
2016-05-02 09:40:11 -0400

    tighten up the carousel

--- wp-content/themes/iei/js/script.js
+++ wp-content/themes/iei/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	$("body").addClass("loaded");
 	
 	$(window).on("resize",function(){
-		$(".home iframe.video").height($(".home .image").height());
+		$(".home iframe.video").height($(".home .carousel").height());
+		$(".home .carousel").height($(".home .image").height());
 	}).trigger("resize");
 });
